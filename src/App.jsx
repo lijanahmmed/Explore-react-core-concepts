acdc0a194456f4e3b52f40b9c238ad89bb3ff6c6
lijanahmmed <lijanahmmed@gmail.com>
2025-07-05 23:17:41 +0600

    added button function react core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,29 @@ import Players from './Players';
 
 
 function App() {
-  const actors = ['Shakib', 'Raj', 'Nisho', 'Shuvo'];
-  const players = [
-    {name : 'Shakib' , age : 40},
-    {name : 'Tamim' , age : 42},
-    {name : 'jakir' , age : 30},
-    {name : 'Das' , age : 32},
-    {name : 'Miraj' , age : 28}
-  ];
-  const books = [
-    {name : 'Biology', price : 150},
-    {name : 'Physics', price : 250},
-    {name : 'Chemistry', price : 500},
-    {name : 'Mtah', price : 450},
-    {name : 'English', price : 350}
-  ]
+  // const actors = ['Shakib', 'Raj', 'Nisho', 'Shuvo'];
+  // const players = [
+  //   {name : 'Shakib' , age : 40},
+  //   {name : 'Tamim' , age : 42},
+  //   {name : 'jakir' , age : 30},
+  //   {name : 'Das' , age : 32},
+  //   {name : 'Miraj' , age : 28}
+  // ];
+  // const books = [
+  //   {name : 'Biology', price : 150},
+  //   {name : 'Physics', price : 250},
+  //   {name : 'Chemistry', price : 500},
+  //   {name : 'Mtah', price : 450},
+  //   {name : 'English', price : 350}
+  // ];
+
+
 
   return (
     <>
       <h2>Vite + React</h2>
-      <Person></Person>
+
+      {/* <Person></Person>
       <Todo task='learn html' isDone= {true}></Todo>
       <Todo task='learn react' isDone= {false}></Todo>
       <Student name="Lijan" age='21'></Student>
@@ -33,38 +36,50 @@ function App() {
       <Actors actors={actors}></Actors>
 
       <BookStore books={books}></BookStore>
-      <Players players={players}></Players>
+      <Players players={players}></Players> */}
+
+
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => alert('button again clicked')}>Click me again</button>
+      <button onClick={() => handleClickAgain(5)}>click me more</button>
 
     </>
   )
 }
 
-function Person() {
-  const age = 21;
-  const money = 100;
-  const person = { name: 'Lijan', address: 'Dhaka'}
-  return <h3>I am {person.name} with age : {age}. I live in {person.address}. I have {money}Tk </h3>
+function handleClick(){
+  alert('button clicked')
+}
+const handleClickAgain = (num) => {
+  alert(num + 3)
 }
 
-function Student(props) {
-  return (
-    <div className='student'>
-      <h3>This is student</h3>
-      <h4>Name: {props.name}</h4>
-      <h4>age: {props.age}</h4>
-    </div>
-  )
-}
+// function Person() {
+//   const age = 21;
+//   const money = 100;
+//   const person = { name: 'Lijan', address: 'Dhaka'}
+//   return <h3>I am {person.name} with age : {age}. I live in {person.address}. I have {money}Tk </h3>
+// }
 
-const {name, price} = {name: 'laptop', price: 50000}
-function Device({name, price}){
-  return(
-    <div className='device'>
-      <h2>This is device</h2>
-      <h3>Name: {name}</h3>
-      <h3>Price: {price}</h3>
-    </div>
-  )
-}
+// function Student(props) {
+//   return (
+//     <div className='student'>
+//       <h3>This is student</h3>
+//       <h4>Name: {props.name}</h4>
+//       <h4>age: {props.age}</h4>
+//     </div>
+//   )
+// }
+
+// const {name, price} = {name: 'laptop', price: 50000}
+// function Device({name, price}){
+//   return(
+//     <div className='device'>
+//       <h2>This is device</h2>
+//       <h3>Name: {name}</h3>
+//       <h3>Price: {price}</h3>
+//     </div>
+//   )
+// }
 
 export default App
